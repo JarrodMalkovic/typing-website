d89c5ac426bcbb6b11064b98288243fb3645d215
furbukit <matt-j-walsh@outlook.com>
2021-08-30 16:27:40 +0800

    Changed styling of homepage+challenge pages, changed title to KeyKorea

--- client/src/pages/challenge/index.js
+++ client/src/pages/challenge/index.js
@@ -8,38 +8,64 @@ import {
   Button,
   useColorModeValue,
   ButtonGroup,
+  Image,
+  Box,
 } from '@chakra-ui/react';
 
 import NextLink from 'next/link';
 
 const Challenge = () => {
   return (
-    <Container pt="8" maxW="container.md">
-      <VStack spacing={8}>
-        <VStack>
-          <Heading>Challenge Mode</Heading>
-          <Text
-            fontSize="lg"
-            align="center"
-            color={useColorModeValue('gray.600', 'gray.200')}>
-            This mode puts your korean typing skills to the ultimate test!
-            Challenge Mode tests all your skills, containing exercises on
-            Letters, Sylabbles, Words, Short Sentances, Long Sentances and
-            Diction!
-          </Text>
-        </VStack>
-        <ButtonGroup>
-          <Button size="sm" bgColor="blue.400" color="white">
+    <Box position="relative">
+      <VStack spacing={5} width="100%">
+        <Image
+          minW="full"
+          opacity="70%"
+          linear-gradient="(to top, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.25))"
+          src="/images/Basic.png"
+        />
+        <Heading position="absolute" top={0}>
+          Challenge Mode
+        </Heading>
+        <Text
+          fontSize="lg"
+          top={20}
+          align="center"
+          position="absolute"
+          color={useColorModeValue('gray.600', 'gray.200')}
+          maxW="container.md">
+          This mode puts your korean typing skills to the ultimate test!
+          Challenge Mode tests all your skills, containing exercises on Letters,
+          Sylabbles, Words, Short Sentances, Long Sentances and Diction!{' '}
+          <br></br>You can track how you go, and enter a leaderboard with your
+          other peers! <br></br>Good Luck!
+        </Text>
+        <ButtonGroup position="absolute" top={80}>
+          <Button
+            size="sm"
+            variant="ghost"
+            bgColor="rgba(200,16,46,0.7)"
+            _hover={{
+              bg: 'rgba(0,47,108,0.7)',
+            }}
+            color="white">
             Start Challenge
           </Button>
           <NextLink href="/practice">
-            <Button size="sm" variant="ghost">
+            <Button
+              size="sm"
+              variant="ghost"
+              bgColor="rgba(200,16,46,0.7)"
+              _hover={{
+                bg: 'rgba(0,47,108,0.7)',
+              }}
+              color="white">
               I need some practice first
             </Button>
           </NextLink>
         </ButtonGroup>
       </VStack>
-    </Container>
+    </Box>
   );
 };
 
