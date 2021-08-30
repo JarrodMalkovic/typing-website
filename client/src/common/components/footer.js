365adf154c67443f0cdf3453d0415c8dac936227
furbukit <matt-j-walsh@outlook.com>
2021-08-30 13:41:29 +0800

    Added some styling to the homepage, including some images.

--- client/src/common/components/footer.js
+++ client/src/common/components/footer.js
@@ -13,7 +13,10 @@ import ActiveNavLink from './active-nav-link';
 // Adapted from: https://chakra-templates.dev/page-sections/footer
 const Footer = () => {
   return (
-    <Box mt={'20'} color={useColorModeValue('gray.700', 'gray.200')}>
+    <Box
+      mt={'20'}
+      color={useColorModeValue('gray.700', 'gray.200')}
+      margin={'0'}>
       <Container
         as={Stack}
         maxW={'container.xl'}
@@ -24,9 +27,9 @@ const Footer = () => {
         align={{ base: 'center', md: 'center' }}>
         <Text>© 2021 Korean Typing Website. All rights reserved</Text>
         <Stack direction={'row'} spacing={6}>
-          <ActiveNavLink name='Terms of Service' href='/terms-of-service' />
-          <ActiveNavLink name='Privacy Policy' href='/privacy-policy' />
-          <ActiveNavLink name='Contact Us' href='/contact-us' />
+          <ActiveNavLink name="Terms of Service" href="/terms-of-service" />
+          <ActiveNavLink name="Privacy Policy" href="/privacy-policy" />
+          <ActiveNavLink name="Contact Us" href="/contact-us" />
         </Stack>
       </Container>
     </Box>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,24 +1,55 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import { Container, Heading, VStack } from '@chakra-ui/react';
-import { Grid, Row, Col } from "react-bootstrap";
+import {
+  Container,
+  Heading,
+  VStack,
+  Image,
+  Box,
+  useColorModeValue,
+  Text,
+} from '@chakra-ui/react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
-
-
-const Home = () => {
+const Home = (props) => {
   return (
-  
-
-
-
-    <Container pt='8' maxW='container.xl'>
-      <VStack spacing={5} width='100%' align='stretch'>
-        <Heading>Homepage</Heading>
+    <Box>
+      <VStack spacing={5} width="100%">
+        <Box position="relative">
+          <Image
+            minW="full"
+            opacity="90%"
+            src={useColorModeValue(
+              '/images/HomeDay.png',
+              '/images/HomeNight.png',
+            )}
+          />
+          <Text
+            position="absolute"
+            fontSize="8vw"
+            fontWeight="bold"
+            fontFamily="Homepage"
+            top="20%"
+            left="5%"
+            transform="translate(-50% -50%)"
+            textColor="white">
+            안녕하십니까
+          </Text>
+          <Text
+            position="absolute"
+            fontSize="6vw"
+            fontWeight="bold"
+            top="30%"
+            left="5%"
+            trasnform="translate(-50% -50%)"
+            textColor="white">
+            {props.username}
+          </Text>
+        </Box>
       </VStack>
-    </Container>
+    </Box>
   );
 };
 
 export default Home;
-
-
