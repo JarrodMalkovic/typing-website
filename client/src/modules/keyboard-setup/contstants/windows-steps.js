08a67bdeace415f506344f8f40eca252a68e52c6
Sherwinli <80800573+Sherwinli@users.noreply.github.com>
2021-08-30 11:45:00 +0800

    need to cover the 4 original files
    for the keyboard setting introduction

--- client/src/modules/keyboard-setup/contstants/windows-steps.js
+++ client/src/modules/keyboard-setup/contstants/windows-steps.js
@@ -5,27 +5,9 @@ import { Flex, Text } from '@chakra-ui/react';
 const Step1 = (
   <Flex>
     <Text>
-      Lorem ipsum odor amet, consectetuer adipiscing elit. Convallis eleifend
-      facilisi at pretium a et fringilla. Auctor justo habitant ullamcorper
-      taciti et magnis. Donec platea etiam tortor nec sem maximus lacinia
-      nascetur. Potenti aliquet urna etiam taciti aliquet pretium mi taciti
-      diam. Pulvinar egestas tortor viverra dapibus ante; condimentum dictum
-      porta. Urna volutpat eleifend praesent maximus iaculis litora posuere.
-      Adipiscing nec dictumst dignissim et egestas bibendum placerat. Lorem
-      ipsum odor amet, consectetuer adipiscing elit. Convallis eleifend facilisi
-      at pretium a et fringilla. Auctor justo habitant ullamcorper taciti et
-      magnis. Donec platea etiam tortor nec sem maximus lacinia nascetur.
-      Potenti aliquet urna etiam taciti aliquet pretium mi taciti diam. Pulvinar
-      egestas tortor viverra dapibus ante; condimentum dictum porta. Urna
-      volutpat eleifend praesent maximus iaculis litora posuere. Adipiscing nec
-      dictumst dignissim et egestas bibendum placerat. Lorem ipsum odor amet,
-      consectetuer adipiscing elit. Convallis eleifend facilisi at pretium a et
-      fringilla. Auctor justo habitant ullamcorper taciti et magnis. Donec
-      platea etiam tortor nec sem maximus lacinia nascetur. Potenti aliquet urna
-      etiam taciti aliquet pretium mi taciti diam. Pulvinar egestas tortor
-      viverra dapibus ante; condimentum dictum porta. Urna volutpat eleifend
-      praesent maximus iaculis litora posuere. Adipiscing nec dictumst dignissim
-      et egestas bibendum placerat.
+      Click on the ‘’magnifying glass icon’’ or ‘’Search Windows box’’ on the left side of your taskbar. (If you can’t find it, right-click on the taskbar and choose Search -> Show search icon).
+               
+      Type “add a language” into the search box. Click on the ‘’Add a language to this device’’ under System settings marked with a gear icon.
     </Text>
   </Flex>
 );
@@ -33,92 +15,29 @@ const Step1 = (
 const Step2 = (
   <Flex>
     <Text>
-      Lorem ipsum odor amet, consectetuer adipiscing elit. Convallis eleifend
-      facilisi at pretium a et fringilla. Auctor justo habitant ullamcorper
-      taciti et magnis. Donec platea etiam tortor nec sem maximus lacinia
-      nascetur. Potenti aliquet urna etiam taciti aliquet pretium mi taciti
-      diam. Pulvinar egestas tortor viverra dapibus ante; condimentum dictum
-      porta. Urna volutpat eleifend praesent maximus iaculis litora posuere.
-      Adipiscing nec dictumst dignissim et egestas bibendum placerat. Lorem
-      ipsum odor amet, consectetuer adipiscing elit. Convallis eleifend facilisi
-      at pretium a et fringilla. Auctor justo habitant ullamcorper taciti et
-      magnis. Donec platea etiam tortor nec sem maximus lacinia nascetur.
-      Potenti aliquet urna etiam taciti aliquet pretium mi taciti diam. Pulvinar
-      egestas tortor viverra dapibus ante; condimentum dictum porta. Urna
-      volutpat eleifend praesent maximus iaculis litora posuere. Adipiscing nec
-      dictumst dignissim et egestas bibendum placerat.
+      Under “Region & language” tab click the “+ Add a language”.
+               
+      In "ADD A LANGUAGE" settings choose “한국어 Korean” or any other language you desire.
     </Text>
+               
+    <img src="/images/windows 1.png" height={500} width={500}/>
+               
+    <img src="/images/windows 2.png" height={500} width={500}/>
   </Flex>
 );
 
 const Step3 = (
   <Flex>
     <Text>
-      Lorem ipsum odor amet, consectetuer adipiscing elit. Convallis eleifend
-      facilisi at pretium a et fringilla. Auctor justo habitant ullamcorper
-      taciti et magnis. Donec platea etiam tortor nec sem maximus lacinia
-      nascetur. Potenti aliquet urna etiam taciti aliquet pretium mi taciti
-      diam. Pulvinar egestas tortor viverra dapibus ante; condimentum dictum
-      porta. Urna volutpat eleifend praesent maximus iaculis litora posuere.
-      Adipiscing nec dictumst dignissim et egestas bibendum placerat. Lorem
-      ipsum odor amet, consectetuer adipiscing elit. Convallis eleifend facilisi
-      at pretium a et fringilla. Auctor justo habitant ullamcorper taciti et
-      magnis. Donec platea etiam tortor nec sem maximus lacinia nascetur.
-      Potenti aliquet urna etiam taciti aliquet pretium mi taciti diam. Pulvinar
-      egestas tortor viverra dapibus ante; condimentum dictum porta. Urna
-      volutpat eleifend praesent maximus iaculis litora posuere. Adipiscing nec
-      dictumst dignissim et egestas bibendum placerat. Lorem ipsum odor amet,
-      consectetuer adipiscing elit. Convallis eleifend facilisi at pretium a et
-      fringilla. Auctor justo habitant ullamcorper taciti et magnis. Donec
-      platea etiam tortor nec sem maximus lacinia nascetur. Potenti aliquet urna
-      etiam taciti aliquet pretium mi taciti diam. Pulvinar egestas tortor
-      viverra dapibus ante; condimentum dictum porta. Urna volutpat eleifend
-      praesent maximus iaculis litora posuere. Adipiscing nec dictumst dignissim
-      et egestas bibendum placerat.
-    </Text>
-  </Flex>
-);
-
-const Step4 = (
-  <Flex>
-    <Text>
-      Ldem ipsum odor amet, consectetuer adipiscing elit. Convallis eleifend
-      facilisi at pretium a et fringilla. Auctor justo habitant ullamcorper
-      taciti et magnis. Donec platea etiam tortor nec sem maximus lacinia
-      nascetur. Potenti aliquet urna etiam taciti aliquet pretium mi taciti
-      diam. Pulvinar egestas tortor viverra dapibus ante; condimentum dictum
-      porta. Urna volutpat eleifend praesent maximus iaculis litora posuere.
-      Adipiscing nec dictumst dignissim et egestas bibendum placerat. Lorem
-      ipsum odor amet, consectetuer adipiscing elit. Convallis eleifend facilisi
-      at pretium a et fringilla. Auctor justo habitant ullamcorper taciti et
-      magnis. Donec platea etiam tortor nec sem maximus lacinia nascetur.
-      Potenti aliquet urna etiam taciti aliquet pretium mi taciti diam. Pulvinar
-      egestas tortor viverra dapibus ante; condimentum dictum porta. Urna
-      volutpat eleifend praesent maximus iaculis litora posuere. Adipiscing nec
-      dictumst dignissim et egestas bibendum placerat. Lorem ipsum odor amet,
-      consectetuer adipiscing elit. Convallis eleifend facilisi at pretium a et
-      fringilla. Auctor justo habitant ullamcorper taciti et magnis. Donec
-      platea etiam tortor nec sem maximus lacinia nascetur. Potenti aliquet urna
-      etiam taciti aliquet pretium mi taciti diam. Pulvinar egestas tortor
-      viverra dapibus ante; condimentum dictum porta. Urna volutpat eleifend
-      praesent maximus iaculis litora posuere. Adipiscing nec dictumst dignissim
-      et egestas bibendum placerat. Lorem ipsum odor amet, consectetuer
-      adipiscing elit. Convallis eleifend facilisi at pretium a et fringilla.
-      Auctor justo habitant ullamcorper taciti et magnis. Donec platea etiam
-      tortor nec sem maximus lacinia nascetur. Potenti aliquet urna etiam taciti
-      aliquet pretium mi taciti diam. Pulvinar egestas tortor viverra dapibus
-      ante; condimentum dictum porta. Urna volutpat eleifend praesent maximus
-      iaculis litora posuere. Adipiscing nec dictumst dignissim et egestas
-      bibendum placerat.
+      A pop-up should appear with the notification saying “We are adding a new feature to Windows.”. Wait for a bit. This alert prompts you to adjust some settings. Unless you have some specific needs and especially if it’s your first time using Korean keyboard you may just leave everything as it is. Default settings are the most commonly used ones. We suggest you choose 2-Set Korean because it is the only national standard for Hangul keyboard.
     </Text>
   </Flex>
 );
 
 const windowsSteps = [
-  { label: 'Step 1', description: 'Step 1 description', content: Step1 },
-  { label: 'Step 2', description: 'Step 2 description', content: Step2 },
-  { label: 'Step 3', description: 'Step 3 description', content: Step3 },
-  { label: 'Step 4', description: 'Step 4 description', content: Step4 },
+  { label: 'Step 1', description: 'Starting', content: Step1 },
+  { label: 'Step 2', description: 'Setting', content: Step2 },
+  { label: 'Step 3', description: 'final', content: Step3 },
 ];
 
 export { windowsSteps };
