143286bc1becef0f785ebe1a8e279e4750220157
22732266 <82798260+22732266@users.noreply.github.com>
2021-09-02 20:28:04 +0800

    Logo update

--- client/src/common/components/logo-svg.js
+++ client/src/common/components/logo-svg.js
@@ -1,11 +1,7 @@
 import * as React from 'react';
 
-<<<<<<< Updated upstream
-import { useColorModeValue, useTheme, Image,Text, HStack } from '@chakra-ui/react';
-=======
-import { useColorModeValue, useTheme, Container, Image} from '@chakra-ui/react';
->>>>>>> Stashed changes
 
+import { useColorModeValue, useTheme, Image,Text, HStack } from '@chakra-ui/react';
 import Link from 'next/link';
 
 // Current SVG is just a placeholder for now
@@ -15,18 +11,12 @@ const Logo = () => {
   return (
     <Link href='/'>
       <a>
-<<<<<<< Updated upstream
         <HStack>
           <Image src="/images/logo.png"  boxSize="50px"/>
           <Text fontWeight =  "bold" fontSize = "2xl"> 
             Key Korea
           </Text>
         </HStack>
-=======
-        <Container padding = "20px">
-          <Image src="/images/logo.png"  boxSize="50px"/>
-        </Container>
->>>>>>> Stashed changes
       </a>
     </Link>
   );
