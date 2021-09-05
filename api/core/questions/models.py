76f68b968051fd1824f790072a0464303cbfd35a
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2021-09-05 21:08:57 +0800

    Added routes for add questions

--- api/core/questions/models.py
+++ api/core/questions/models.py
@@ -7,6 +7,7 @@ class Question(models.Model):
     subexercise_slug = models.ForeignKey(Subexercise, on_delete=models.CASCADE)
     question = models.CharField(max_length=100)
     audio_url = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.question}"
@@ -15,24 +16,25 @@ class Question(models.Model):
 class PracticeAttempt(models.Model):
     attempt = models.IntegerField()
     subexercise_slug = models.ForeignKey(Subexercise, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     wpm = models.FloatField()
     time_elapsed = models.DateTimeField()
     accuracy = models.FloatField()
     score = models.FloatField()
-    
+
     def __str__(self):
         return f"{self.attempt}"
-    
+
 
 class ChallengeAttempt(models.Model):
     attempt = models.IntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     wpm = models.FloatField()
     time_elapsed = models.DateTimeField()
     accuracy = models.FloatField()
     score = models.FloatField()
-    
-    
+
     def __str__(self):
         return f"{self.attempt}"
