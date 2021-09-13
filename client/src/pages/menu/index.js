4197b2d38ef9aad076cb6d189ee6073f01c2b6e8
22732266 <82798260+22732266@users.noreply.github.com>
2021-09-13 22:32:59 +0800

    update

--- client/src/pages/menu/index.js
+++ client/src/pages/menu/index.js
@@ -38,45 +38,45 @@ const Menu = () => {
             Choose Your Path!
         </Text>
 
-        <WrapItem>
-          <HStack>
-            <Tooltip label = "Set your keyboard to Korean!" placement = "bottom">
-              <Container padding = "20px" maxW="container.lg" _hover={{color: "#39aae1"}}>
-                <Link href="/setup">
-                  <a>
-                    <Image src = "/images/setup.png" boxSize="200px" margin = "10px"/>
-                    <Text mt="10px" textAlign = "center" fontWeight =  "bold" fontSize = "xl">Setup</Text>
-                  </a>
-                </Link>
-              </Container>
-            </Tooltip>
+          <WrapItem>
+            <HStack>
+              <Tooltip label = "Set your keyboard to Korean!" placement = "bottom">
+                <Container padding = "20px" maxW="container.lg" _hover={{color: "#39aae1"}}>
+                  <Link href="/setup">
+                    <a>
+                      <Image src = "/images/setup.png" boxSize="200px" margin = "10px"/>
+                      <Text mt="10px" textAlign = "center" fontWeight =  "bold" fontSize = "xl">Setup</Text>
+                    </a>
+                  </Link>
+                </Container>
+              </Tooltip>
 
-            <Tooltip label = "Begin practicing your keyboard typing skills!" placement = "bottom">
-              <Container padding = "20px" maxW="container.lg" _hover={{color: "#39aae1"}}>
-                <Link href="/practice">
-                  <a>
-                    <Image src = "/images/practice.png" boxSize="200px" margin = "10px"/>
-                    <Text mt="10px" textAlign = "center" fontWeight =  "bold" fontSize = "xl">Practice</Text>
-                  </a>
-                </Link>
-              </Container>
-            </Tooltip>
+              <Tooltip label = "Begin practicing your keyboard typing skills!" placement = "bottom">
+                <Container padding = "20px" maxW="container.lg" _hover={{color: "#39aae1"}}>
+                  <Link href="/practice">
+                    <a>
+                      <Image src = "/images/practice.png" boxSize="200px" margin = "10px"/>
+                      <Text mt="10px" textAlign = "center" fontWeight =  "bold" fontSize = "xl">Practice</Text>
+                    </a>
+                  </Link>
+                </Container>
+              </Tooltip>
 
-            <Tooltip label = "Put your typing skills to the ultimate test!" placement = "bottom">
-              <Container padding = "20px" maxW="container.lg" _hover={{color: "#39aae1"}}>
-                <Link href="/challenge">
-                  <a>
-                    <Image src = "/images/challenge.png" boxSize="200px" margin = "10px"/>
-                    <Text mt="10px" textAlign = "center" fontWeight =  "bold" fontSize = "xl">Challenge</Text>
-                  </a>
-                </Link>
-              </Container>
-            </Tooltip>
+              <Tooltip label = "Put your typing skills to the ultimate test!" placement = "bottom">
+                <Container padding = "20px" maxW="container.lg" _hover={{color: "#39aae1"}}>
+                  <Link href="/challenge">
+                    <a>
+                      <Image src = "/images/challenge.png" boxSize="200px" margin = "10px"/>
+                      <Text mt="10px" textAlign = "center" fontWeight =  "bold" fontSize = "xl">Challenge</Text>
+                    </a>
+                  </Link>
+                </Container>
+              </Tooltip>
 
-          </HStack>
-        </WrapItem>
-        </VStack>
-        </Box>
+            </HStack>
+          </WrapItem>
+      </VStack>
+    </Box>
   );
 };
 
