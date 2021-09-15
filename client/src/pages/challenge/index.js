9373092d154e48fa8a26a5965f66f962482daea7
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2021-09-15 14:13:02 +0800

    Retreive questions from backend

--- client/src/pages/challenge/index.js
+++ client/src/pages/challenge/index.js
@@ -8,38 +8,135 @@ import {
   Button,
   useColorModeValue,
   ButtonGroup,
+  Stack,
+  useTheme,
 } from '@chakra-ui/react';
 
 import NextLink from 'next/link';
+import { useQuery, useQueryClient } from 'react-query';
+import axios from 'axios';
+import { BASE_API_URL } from '../../common/contstants/base-api-url';
+import ExerciseContent from '../../modules/exercises/components/exercise-content';
+import ChallengeSummary from '../../modules/exercises/components/challenge-summary';
+import ProgressBar from '@ramonak/react-progress-bar';
+
+const getCompletionPercentage = (currentQuestionIndex, totalQuestions) =>
+  Math.ceil((currentQuestionIndex / totalQuestions) * 100);
+
+const getChallengeQuestions = async () => {
+  const res = await axios.get(`${BASE_API_URL}/api/challenge/`);
+
+  return res.data;
+};
 
 const Challenge = () => {
+  const theme = useTheme();
+  const queryClient = useQueryClient();
+
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [accuracy, setAccuracy] = React.useState(100);
+  const [wordsTyped, setWordsTyped] = React.useState(0);
+  const [startDate, setStartDate] = React.useState(new Date());
+  const [currentQuestionIndex, setCurrentQuestionIndex] = React.useState(0);
+
+  const { data, refetch, isFetching } = useQuery(
+    'challenge',
+    getChallengeQuestions,
+    {
+      refetchOnWindowFocus: false,
+      enabled: false,
+      onSuccess: () => {
+        setIsPlaying(true);
+      },
+    },
+  );
+
+  const restartChallengeMode = () => {
+    queryClient.invalidateQueries('challenge');
+    refetch();
+    setAccuracy(100);
+    setWordsTyped(0);
+    setStartDate(new Date());
+    setCurrentQuestionIndex(0);
+  };
+
   return (
-    <Container pt="8" maxW="container.lg">
-      <VStack spacing={8}>
-        <VStack>
-          <Heading>Challenge Mode</Heading>
-          <Text
-            fontSize="lg"
-            align="center"
-            color={useColorModeValue('gray.600', 'gray.200')}>
-            This mode puts your korean typing skills to the ultimate test!
-            Challenge Mode tests all your skills, containing exercises on
-            Letters, Sylabbles, Words, Short Sentances, Long Sentances and
-            Diction!
-          </Text>
-        </VStack>
-        <ButtonGroup>
-          <Button size="sm" bgColor="blue.400" color="white">
-            Start Challenge
-          </Button>
-          <NextLink href="/practice">
-            <Button size="sm" variant="ghost">
-              I need some practice first
-            </Button>
-          </NextLink>
-        </ButtonGroup>
-      </VStack>
-    </Container>
+    <>
+      {isPlaying ? (
+        <Container pt="8" maxW="container.xl">
+          <ProgressBar
+            bgColor={theme.colors.blue['400']}
+            baseBgColor={useColorModeValue(
+              theme.colors.gray['200'],
+              theme.colors.gray['700'],
+            )}
+            labelColor={useColorModeValue(
+              theme.colors.gray['700'],
+              theme.colors.gray['200'],
+            )}
+            completed={getCompletionPercentage(
+              currentQuestionIndex,
+              data.length,
+            )}
+          />
+          <Stack spacing={8} mx={'auto'} maxW={'3xl'} py={12} px={6}>
+            {currentQuestionIndex >= data.length ? (
+              <ChallengeSummary
+                setCurrentQuestionIndex={setCurrentQuestionIndex}
+                startDate={startDate}
+                wordsTyped={wordsTyped}
+                accuracy={accuracy}
+                refetch={refetch}
+                restartChallengeMode={restartChallengeMode}
+              />
+            ) : (
+              <ExerciseContent
+                questions={data}
+                setCurrentQuestionIndex={setCurrentQuestionIndex}
+                currentQuestionIndex={currentQuestionIndex}
+                startDate={startDate}
+                wordsTyped={wordsTyped}
+                setWordsTyped={setWordsTyped}
+                accuracy={accuracy}
+                setAccuracy={setAccuracy}
+              />
+            )}
+          </Stack>
+        </Container>
+      ) : (
+        <Container pt="8" maxW="container.lg">
+          <VStack spacing={8}>
+            <VStack>
+              <Heading>Challenge Mode</Heading>
+              <Text
+                fontSize="lg"
+                align="center"
+                color={useColorModeValue('gray.600', 'gray.200')}>
+                This mode puts your korean typing skills to the ultimate test!
+                Challenge Mode tests all your skills, containing exercises on
+                Letters, Sylabbles, Words, Short Sentances, Long Sentances and
+                Diction!
+              </Text>
+            </VStack>
+            <ButtonGroup>
+              <Button
+                isLoading={isFetching}
+                onClick={refetch}
+                size="sm"
+                bgColor="blue.400"
+                color="white">
+                Start Challenge
+              </Button>
+              <NextLink href="/practice">
+                <Button size="sm" variant="ghost">
+                  I need some practice first
+                </Button>
+              </NextLink>
+            </ButtonGroup>
+          </VStack>
+        </Container>
+      )}
+    </>
   );
 };
 
--- client/src/pages/practice/index.js
+++ client/src/pages/practice/index.js
@@ -17,106 +17,95 @@ import {
 import Link from 'next/link';
 import PracticeExerciseButton from '../../modules/practice/practice-exercise-button';
 
-const sampleData = [
-  {
-    name: 'Letters',
-    img: '/images/Letters.png',
-    subExercises: [
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: true },
-      { name: 'test', slug: 'test', disabled: true },
-    ],
-  },
-  {
-    name: 'Syllables',
-    img: '/images/Syllable.png',
-    subExercises: [
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-    ],
-  },
-  {
-    name: 'Words',
-    img: '/images/Words.png',
-    subExercises: [
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-    ],
-  },
-  {
-    name: 'Short Sentences',
-    img: '/images/Short-Sentences.png',
-    subExercises: [
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-    ],
-  },
-  {
-    name: 'Long Sentences',
-    img: '/images/Long-Sentences.png',
-    subExercises: [
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-    ],
-  },
-  {
-    name: 'Diction',
-    img: '/images/Dictation.png',
-    subExercises: [
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-      { name: 'test', slug: 'test', disabled: false },
-    ],
-  },
-];
+// const sampleData = [
+//   {
+//     name: 'Letters',
+//     img: '/images/Letters.png',
+//     subExercises: [
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: true },
+//       { name: 'test', slug: 'test', disabled: true },
+//     ],
+//   },
+//   {
+//     name: 'Syllables',
+//     img: '/images/Syllable.png',
+//     subExercises: [
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//     ],
+//   },
+//   {
+//     name: 'Words',
+//     img: '/images/Words.png',
+//     subExercises: [
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//     ],
+//   },
+//   {
+//     name: 'Short Sentences',
+//     img: '/images/Short-Sentences.png',
+//     subExercises: [
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//     ],
+//   },
+//   {
+//     name: 'Long Sentences',
+//     img: '/images/Long-Sentences.png',
+//     subExercises: [
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//     ],
+//   },
+//   {
+//     name: 'Diction',
+//     img: '/images/Dictation.png',
+//     subExercises: [
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//       { name: 'test', slug: 'test', disabled: false },
+//     ],
+//   },
+// ];
+
+import { exercises } from '../../common/contstants/exercises';
 
 const Practice = () => {
   return (
-    <Container mt = "50px" maxW="container.lg" centerContent>
-      <VStack>
+    <Container pt="8" maxW="container.lg">
+      <VStack spacing="8">
         <VStack>
-          <Heading fontSize = "45px" padding = "20px">
-            Practice Mode
-          </Heading>
-        </VStack>
-
-        <Divider/>
-
-        <VStack>
-          <Text
-            fontSize= "20px"
-            color={useColorModeValue('gray.600', 'gray.200')}>
-            Choose Your Path!
-          </Text>
+          <Heading>Practice Mode</Heading>
         </VStack>
         <Wrap spacing="30px" justify="center" maxW="container.md">
-          {sampleData.map((data) => (
+          {Object.entries(exercises).map(([key, value], idx) => (
             <PracticeExerciseButton
-              key={data.name}
-              name={data.name}
-              img = {data.img}
-              subExercises={data.subExercises}
+              key={value.name}
+              name={value.name}
+              slug={value.slug}
+              img={value.img}
             />
           ))}
         </Wrap>
-        </VStack>
-      
+      </VStack>
     </Container>
   );
 };
