a69fdb70354b698757775fc8b8f2af2facd62243
Sherwinli <liziwen1997@gmail.com>
2021-09-16 17:16:20 +0800

    Ziwen add the continue button

--- client/src/modules/keyboard-setup/components/steps-completed-prompt.js
+++ client/src/modules/keyboard-setup/components/steps-completed-prompt.js
@@ -60,7 +60,6 @@ const StepsCompletedPrompt = ({ os }) => {
             <Input onChange={handleChange} />
           </Stack>
         </Box>
-
         {
           isKoreanText &&
           <Button >
