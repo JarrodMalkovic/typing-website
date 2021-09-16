1481ca04fe89cc4868b9752d10f320e3bf683254
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2021-09-17 01:54:57 +0800

    Fix for practtice and challenge mode

--- client/src/modules/practice/practice-exercise-button.js
+++ client/src/modules/practice/practice-exercise-button.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import {
   VStack,
+  HStack,
   Text,
   WrapItem,
   Button,
@@ -9,10 +10,12 @@ import {
   Modal,
   ModalOverlay,
   Box,
+  Flex,
   Center,
   ModalContent,
   ModalCloseButton,
   ModalBody,
+  Spinner,
   ModalFooter,
   ModalHeader,
   useDisclosure,
@@ -21,94 +24,107 @@ import {
   AccordionItem,
   AccordionButton,
   AccordionPanel,
-  AccordionIcon
+  AccordionIcon,
 } from '@chakra-ui/react';
-
 import Link from 'next/link';
-
 import PropTypes from 'prop-types';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { BASE_API_URL } from '../../common/contstants/base-api-url';
 
-const PracticeExerciseButton = ({ name,img, subExercises,dscrpt }) => {
+const isSubexerciseDisabled = (subexercises, subexercise, idx) => {
+  if (subexercise.attempt) return false;
+  if (subexercise.level === 1) return false;
+  if (subexercises[idx - 1] && subexercises[idx - 1].attempt) return false;
+
+  return true;
+};
+
+const getSubexercises = async (slug) => {
+  const res = await axios.get(
+    `${BASE_API_URL}/api/subexercises/exercise/${slug}/`,
+  );
+
+  return res.data;
+};
+
+const PracticeExerciseButton = ({ name, img, slug }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { data, isLoading, isFetching } = useQuery(
+    slug,
+    () => getSubexercises(slug),
+    {
+      enabled: isOpen,
+    },
+  );
+
+  console.log(data)
 
   return (
     <>
-      <WrapItem onClick={onOpen} as="button">
-        <VStack>
-          
-          <Container padding = "20px" maxW="container.lg" _hover={{
-                                  color: "#39aae1",
-                                }}>
-            <Image src = {img} boxSize="160px"/>
-            <Text mt="10px" fontWeight =  "bold" fontSize = "xl">{name}</Text>
-          </Container>
+      <WrapItem paddingX="50px">
+        <VStack onClick={onOpen} as="button">
+          <Image src={img} boxSize="135px" />
+          <Center>
+            <Text mt="10px" width="130px" fontWeight="bold" fontSize="xl">
+              {name}
+            </Text>
+          </Center>
         </VStack>
       </WrapItem>
 
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
-
-        
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader fontSize = "30px">
+          <ModalHeader fontSize="30px">
             <Center>{name}</Center>
           </ModalHeader>
-          <ModalBody>
-            <Box textAlign = "center" padding = "0px 20px">{dscrpt}</Box>
-          </ModalBody>
+          <ModalBody></ModalBody>
           <ModalCloseButton />
           <ModalBody>
-            
-            <VStack margin = "10px">
-              {subExercises.map((subExercise) => (
-                <>
-                  <Accordion 
-                    allowMultiple
-                    allowToggle 
-                    width="90%"
-                    >
-                    <AccordionItem>  
-
-                      <h2>
-                        <AccordionButton width="100%">
-                          <Box flex="1" textAlign="left" fontWeight = "bold">
-                            {subExercise.name}
-                          </Box>
-                          <AccordionIcon />
-                        </AccordionButton>
-                      </h2>
-                        
-                     
-
-                      <AccordionPanel pb={4} textAlign = "center">
-                        
-                        <Box textAlign = "center" padding = "0px 20px">{subExercise.description}</Box>
-                        
-
-                        <Link href={`/practice/${subExercise.slug}`}>
-                              <Button
-                                disabled={subExercise.disabled} 
-                                width="50%" 
-                                flex = "1" 
-                                textAlign = "left"
-                                justify
-                                margin = "10px"
-                                _hover={{
-                                  color: "#39aae1",
-                                }}>
-                                Begin
-                              </Button>
-                          </Link>
-
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </Accordion>
-                </> 
-              ))}
-            </VStack>
+            {isLoading || !data ? (
+              <Center>
+                <Spinner />
+              </Center>
+            ) : (
+              <Accordion allowToggle>
+                {data.map((subExercise, idx) => (
+                  <AccordionItem
+                    isDisabled={isSubexerciseDisabled(data, subExercise, idx)}>
+                    <h2>
+                      <AccordionButton>
+                        <Box flex="1" textAlign="left">
+                          {subExercise.subexercise_name}
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel>
+                      <Box textAlign="center" padding="0px 20px">
+                        Placeholder description
+                        <Link
+                          href={`/practice/${subExercise.subexercise_slug}`}>
+                          <Button
+                            width="50%"
+                            flex="1"
+                            textAlign="left"
+                            justify
+                            margin="10px"
+                            _hover={{
+                              color: 'teal.500',
+                            }}>
+                            Begin
+                          </Button>
+                        </Link>
+                      </Box>
+                    </AccordionPanel>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+            )}
           </ModalBody>
           <ModalFooter />
-        </ModalContent> 
+        </ModalContent>
       </Modal>
     </>
   );
@@ -117,6 +133,18 @@ const PracticeExerciseButton = ({ name,img, subExercises,dscrpt }) => {
 PracticeExerciseButton.propTypes = {
   name: PropTypes.string,
   subExercises: PropTypes.array,
+  img: PropTypes.string,
+  slug: PropTypes.string,
 };
+
+// {/* <Link
+//                   key={subExercise.subexercise_slug}
+//                   href={`/practice/${subExercise.subexercise_slug}`}>
+//                   <Button
+//                     disabled={isSubexerciseDisabled(data, subExercise, idx)}
+//                     width="100%">
+//                     {subExercise.subexercise_name}
+//                   </Button>
+//                 </Link> */}
 
 export default PracticeExerciseButton;
--- client/src/pages/practice/index.js
+++ client/src/pages/practice/index.js
@@ -14,81 +14,7 @@ import {
 
 import Link from 'next/link';
 import PracticeExerciseButton from '../../modules/practice/practice-exercise-button';
-
-const sampleData = [
-  {
-    name: 'Letters',
-    img: '/images/Letters.png',
-    dscrpt: 'Practice the alphabets on keyboard!',
-    subExercises: [
-      { name: 'Left Hand', slug: 'test', disabled: false, description: "Practice your consonants!"},
-      { name: 'Right Hand', slug: 'test', disabled: false, description: "Practice your vowels!"},
-      { name: 'Right + Left Hand', slug: 'test', disabled: false, description: "Practice both your consonants and vowels!" },
-      { name: 'Shift + Right Hand', slug: 'test', disabled: true , description: "Practice your double consonants!"},
-      { name: 'Shift + Left Hand', slug: 'test', disabled: true, description: "Practice your double vowels!"},
-      { name: 'Complete', slug: 'test', disabled: true, description: "Put all your knowledge of the Korean Letters to the test!"},
-    ],
-  },
-  {
-    name: 'Syllables',
-    img: '/images/Syllable.png',
-    dscrpt: 'Create words using the letters introduced in previous module!',
-    subExercises: [
-      { name: 'C + V', slug: 'test', disabled: false, 
-        description: "This exercise will focus on words with a consonant and a vowel."},
-      { name: 'Shift C + V', slug: 'test', disabled: false, 
-        description: "This exercise will focus on words with a double consontant and a vowel."},
-      { name: 'C + V + C', slug: 'test', disabled: false,
-        description: "This exercise will focus on words with a consonant, a vowel, then another consonant." },
-      { name: 'Shift C + V + C', slug: 'test', disabled: false,
-        description: "This exercise will focus on words with a double consonant, a vowel, then a consonant." },
-      { name: 'C + V + CC', slug: 'test', disabled: false,
-        description: "This exercise will focus on words with a consonant, a vowel, then followed by two consonants." },
-    ],
-  },
-  {
-    name: 'Words',
-    img: '/images/Words.png',
-    dscrpt: 'Practice basic Korean words!',
-    subExercises: [
-      { name: '2 Syllables With Ending Consonant', slug: 'test', disabled: false },
-      { name: '2 Syllables Without Ending Consonant', slug: 'test', disabled: false },
-      { name: '3 Syllables With Ending Consonant', slug: 'test', disabled: false },
-      { name: '3 Syllables Without Ending Consonant', slug: 'test', disabled: false },
-      { name: 'Longer than 3 syllables', slug: 'test', disabled: false },
-    ],
-  },
-  {
-    name: 'Short Sentences',
-    img: '/images/Short-Sentences.png',
-    dscrpt: 'Learn these basic Korean phrases!',
-    subExercises: [
-      { name: 'Short Sentences', slug: 'test', disabled: false }
-    ],
-  },
-  {
-    name: 'Long Sentences',
-    img: '/images/Long-Sentences.png',
-    dscrpt: 'Feeling confident with your progress so far? Attempt this module to expand your horizon!',
-    subExercises: [
-      { name: 'Long Sentences', slug: 'test', disabled: false }
-    ],
-  },
-  {
-    name: 'Diction',
-    img: '/images/Dictation.png',
-    dscrpt: 'Practice your listening skills!',
-    subExercises: [
-      { name: 'Dictation', slug: 'test', disabled: false, 
-        description: 'Practice listening to words!'},
-      { name: 'Short Sentence Dictation', slug: 'test', disabled: false, 
-        description: 'Practice listening to short sentences!' },
-      { name: 'Long Sentence Dictation', slug: 'test', disabled: false, 
-        description: 'Practice listening to long sentences!'},
-      
-    ],
-  },
-];
+import { exercises } from '../../common/contstants/exercises';
 
 const Practice = () => {
   return (
@@ -118,14 +44,12 @@ const Practice = () => {
           maxW="container.md"
           position="absolute"
           top={40}>
-          {sampleData.map((data) => (
+          {Object.entries(exercises).map(([key, value], idx) => (
             <PracticeExerciseButton
-              key={data.name}
-              name={data.name}
-              img = {data.img}
-              description = {data.description}
-              dscrpt = {data.dscrpt}
-              subExercises={data.subExercises}
+              key={value.name}
+              name={value.name}
+              slug={value.slug}
+              img={value.img}
             />
           ))}
         </Wrap>
