532415adf846430e6a69aaabcf8b228466ddb90b
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2021-09-29 23:07:54 +0800

    Created exercise serializers

---
+++ core/exercises/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Exercise
+
+
+class CreateExerciseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Exercise
+        fields = ['exercise_name', 'description', 'image',
+                  'exercise_slug', 'allow_audio_files_in_questions', 'hidden', 'allow_in_challenge_mode', 'created_at']
+
+
+class UpdateExerciseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Exercise
+        fields = ['exercise_name', 'description',
+                  'allow_audio_files_in_questions', 'hidden', 'allow_in_challenge_mode', 'created_at']
