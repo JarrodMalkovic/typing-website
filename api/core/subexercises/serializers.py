3964355f99a303f69a5510792eef2984f7cfa35c
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2021-09-29 23:09:27 +0800

    Created subexercise serializer

---
+++ api/core/subexercises/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Subexercise
+
+
+class SubexerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subexercise
+        fields = ['subexercise_slug',
+                  'subexercise_name', 'level', 'exercise_slug']
