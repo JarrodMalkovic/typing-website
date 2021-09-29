c6df2ec8af50e5d24ea1d052f960be17e02ac3ea
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2021-09-29 23:11:04 +0800

    Added swagger for API Docs

--- api/myapi/urls.py
+++ api/myapi/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="KeyKorea API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger',
+                                         cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include(('core.routers', 'core'), namespace='core-api')),
 ]
