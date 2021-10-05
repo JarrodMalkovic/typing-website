84d58093416ed4addb4e0bd15f8f49ae7f704f3f
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2021-10-05 16:21:46 +0800

    Added excel upload

---
+++ client/src/modules/dashboard/excel-upload.js
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import * as XLSX from 'xlsx';
+import { useDropzone } from 'react-dropzone';
+import { Text, Box, Flex } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+
+const getColor = ({ isDragAccept, isDragReject, isDragActive }) => {
+  if (isDragAccept) {
+    return '#00e676';
+  }
+
+  if (isDragReject) {
+    return '#ff1744';
+  }
+
+  if (isDragActive) {
+    return '#2196f3';
+  }
+
+  return 'rgba(255, 255, 255, 0.16)';
+};
+
+const ExcelUpload = ({ setFieldValue }) => {
+  const [file, setFile] = React.useState(null);
+
+  const readExcel = (file) => {
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file);
+
+    fileReader.onload = (e) => {
+      const bufferArray = e.target.result;
+      const wb = XLSX.read(bufferArray, { type: 'buffer' });
+      const questions = [];
+
+      wb.SheetNames.forEach((wsName) => {
+        const ws = wb.Sheets[wsName];
+        const data = [{ exercise: wsName }];
+        data.push(XLSX.utils.sheet_to_json(ws));
+        questions.push(data);
+      });
+      console.log(questions);
+      setFieldValue('questions', questions);
+    };
+
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  };
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    accept: '',
+    onDrop: (acceptedFiles) => {
+      setFile(acceptedFiles[0]);
+      readExcel(acceptedFiles[0]);
+    },
+  });
+
+  return (
+    <Box>
+      <Flex
+        display="flex"
+        textAlign="center"
+        padding="30px"
+        borderRadius="5px"
+        color="#d1d5db"
+        border="dashed"
+        borderWidth="2px"
+        borderColor={getColor({
+          isDragActive,
+          isDragAccept,
+          isDragReject,
+        })}
+        {...getRootProps({
+          isDragActive,
+          isDragAccept,
+          isDragReject,
+        })}>
+        <input {...getInputProps()} />
+
+        <Text size="sm">
+          Drag and drop some files here, or click to select files
+        </Text>
+      </Flex>
+      {file && (
+        <Text fontSize="sm" mt="2">
+          {file.path} - {file.size}kb
+        </Text>
+      )}
+    </Box>
+  );
+};
+
+ExcelUpload.propTypes = {
+  setFieldValue: PropTypes.func,
+};
+
+export default ExcelUpload;
--- client/src/modules/dashboard/upload-questions-button.js
+++ client/src/modules/dashboard/upload-questions-button.js
@@ -1,53 +1,62 @@
 import * as React from 'react';
 import { Button } from '@chakra-ui/react';
 import { ArrowUpIcon } from '@chakra-ui/icons';
-import * as FileSaver from "file-saver";
-import * as XLSX from "xlsx";
-import axios from 'axios'
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+import axios from 'axios';
 import { BASE_API_URL } from '../../common/contstants/base-api-url';
+import { useAuth } from '../auth/hooks/use-auth';
 
-// need to add authentication
-
-async function getExercises(){
-  const {data} = await axios.get(`${BASE_API_URL}/api/exercises/`)
+const getExercises = async () => {
+  const { data } = await axios.get(`${BASE_API_URL}/api/exercises/`);
   return data;
-}
+};
 
-async function getQuestions(slug){
-  const {data} = await axios.get(`${BASE_API_URL}/api/download-questions/${slug}/`)
+const getQuestions = async (slug) => {
+  const { data } = await axios.get(
+    `${BASE_API_URL}/api/download-questions/${slug}/`,
+  );
+
   return data;
-}
+};
 
-const ExportToExcel = ({fileName}) => {
-  const fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-  const fileExtension = ".xlsx";
-  const [wb, setWb] = React.useState({SheetNames: [], Sheets: []})
+const ExportToExcel = ({ fileName }) => {
+  const { state } = useAuth();
+
+  const fileType =
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+
+  const fileExtension = '.xlsx';
+  const [wb, setWb] = React.useState({ SheetNames: [], Sheets: [] });
 
   React.useEffect(async () => {
-    const currentWb = {SheetNames: [], Sheets: []}
+    if (!state.isAuthenticated) {
+      return;
+    }
+
+    const currentWb = { SheetNames: [], Sheets: [] };
     const exercises = await getExercises();
 
     // for each exercise, create a worksheet and append that to the workbook
-    for(const exercise of exercises) {
+    for (const exercise of exercises) {
       const exercise_name = exercise['exercise_name'];
       const exercise_slug = exercise['exercise_slug'];
-      
+
       // Does not work with dictation
-      if(exercise_name != "Diction") {
+      if (exercise_name != 'Diction') {
         const questions = await getQuestions(exercise_slug);
 
         const currentWs = XLSX.utils.json_to_sheet(questions);
         currentWb.SheetNames.push(exercise_name);
         currentWb.Sheets[exercise_name] = currentWs;
-      };
-    };
-    
-    setWb(currentWb)
-  }, [])
-  
+      }
+    }
+
+    setWb(currentWb);
+  }, [state.isAuthenticated]);
 
   const exportToCSV = (fileName) => {
-    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
     const data = new Blob([excelBuffer], { type: fileType });
     FileSaver.saveAs(data, fileName + fileExtension);
   };
@@ -59,8 +68,7 @@ const ExportToExcel = ({fileName}) => {
         color="white"
         onClick={(e) => exportToCSV(fileName)}
         leftIcon={<ArrowUpIcon />}
-        variant="solid"
-        >
+        variant="solid">
         Upload Questions
       </Button>
     </>
