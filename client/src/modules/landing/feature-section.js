87223129398e0bdac623ab8495c4fce9ae6afd28
anders-m-c <christensen.anders945@gmail.com>
2021-10-18 13:51:30 +0800

    Changed home page blurb

--- client/src/modules/landing/feature-section.js
+++ client/src/modules/landing/feature-section.js
@@ -22,13 +22,11 @@ const FeatureSection = ({ learnMoreRef }) => {
             h="16"
             src="/images/challenge-small-icon.webp"
           />
-          <Text fontWeight={600}>Who are we?</Text>
+          <Text fontWeight={600}>Who We Are</Text>
           <Text color={useColorModeValue('gray.600', 'gray.400')}>
-            KeyKorea is a student designed website to help you learn the Korean
-            Keyboard. Whether you are just starting out, or want some extra
-            practice, KeyKorea is the best place for you to learn! With an
-            intuitive and easy to use interface, you will be a master of the
-            Korean Keyboard in no time!
+            KeyKorea is a website ideated by Dr Nicola Fraschini and created by computer science students
+            Jarrod Malkovic, Anders Christensen, Diana Nik Suzaimi, Anoushka Singh, 
+            Matthew Walsh and Ziwen Li at the University of Western Australia.
           </Text>
         </Stack>
         <Stack textAlign={{ base: 'center', lg: 'justify' }}>
@@ -41,11 +39,13 @@ const FeatureSection = ({ learnMoreRef }) => {
           />
           <Text fontWeight={600}> Designed For Your Success</Text>
           <Text color={useColorModeValue('gray.600', 'gray.400')}>
-            Everyone learns in different ways. For the first time in history, we
-            can analyze how millions of people learn at once to create the most
-            effective educational system possible and tailor it to each student.
-            Our ultimate goal is to give everyone access to a private tutor
-            experience through technology.
+            KeyKorea is designed to help you learning typing in Korean. It is 
+            has been thought for learners of Korean as a foreign language, and 
+            it is structured with practice exercises of increasing difficulty to 
+            help you become a master of the Korean keyboard. All of the words and sentences 
+            are at the level of beginner or intermediate learner of Korean and appears 
+            with their English translation, so you can also improve your Korean while 
+            learning how to type it!
           </Text>
         </Stack>
         <Stack textAlign={{ base: 'center', lg: 'justify' }}>
@@ -56,12 +56,11 @@ const FeatureSection = ({ learnMoreRef }) => {
             h="16"
             src="/images/keyboard-small-icon.webp"
           />
-          <Text fontWeight={600}> Enhance Your Korean Typing Skills</Text>
+          <Text fontWeight={600}>Enhance Your Korean Typing Skills</Text>
           <Text color={useColorModeValue('gray.600', 'gray.400')}>
-            A passion for learning is the heart of everything we do. KeyKorea
-            allows you to work through examples directly set by your lecturer!
-            Either in our chill practice mode, or our challenge mode, where you
-            can compete against other students on a global leaderboard!
+            You can select the practice mode if you think that you need to do 
+            some more practice. Once you feel confident, you can attempt the challenge
+            mode to check your progress and compare your score with other students.
           </Text>
         </Stack>
       </SimpleGrid>
