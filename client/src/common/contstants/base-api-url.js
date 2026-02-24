c849a2f3145b5ddc329af39e91cee3d355fb2112
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2026-02-24 19:03:40 +0800

    Migrate hosting + DB away from Heroku due to https://finance.yahoo.com/news/salesforce-halts-development-features-heroku-141304123.html  (new DB on Neon with latest Heroku backup in prod)

--- client/src/common/contstants/base-api-url.js
+++ client/src/common/contstants/base-api-url.js
@@ -1,4 +1,4 @@
 export const BASE_API_URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? 'https://keykorea.herokuapp.com'
-    : 'https://keykorea.herokuapp.com';
+    ? 'https://typing-website-lk0i.onrender.com'
+    : 'https://typing-website-lk0i.onrender.com';
