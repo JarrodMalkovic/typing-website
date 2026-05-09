63a5f3312a81e5be70a787f9955ac01f340fe1ee
Jarrod Malkovic <jarrodmalkovic@outlook.com>
2026-05-09 14:19:18 +0800

    update api url

--- client/src/common/contstants/base-api-url.js
+++ client/src/common/contstants/base-api-url.js
@@ -1,4 +1,4 @@
 export const BASE_API_URL =
   process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-    ? 'https://typing-website-lk0i.onrender.com'
-    : 'https://typing-website-lk0i.onrender.com';
+    ? 'https://typing-website-production.up.railway.app'
+    : 'https://typing-website-production.up.railway.app';
